5b0c77c990d680b13a2b80cd0ec53f1b47abdf86
fantchenou.pierremarie <fantchenou.pierremarie@gmail.com>
2018-09-25 11:57:35 -0400

    configure simpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,9 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+require 'simplecov'
+SimpleCov.start
   # Add more helper methods to be used by all tests here...
 end
 
-require 'simplecov'
-SimpleCov.start
+
